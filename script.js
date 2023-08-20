fe78053cc2dcf03c65075a53b0b4fe80a22e2bee
Muhammad Hussain Javed <k213584@nu.edu.pk>
2023-08-20 20:03:47 +0500

    Added some button containers

--- script.js
+++ script.js
@@ -10,4 +10,50 @@ const gameBoard = (() => {
     };  
 })();
 
+// Create a module which starts the game
+
+const PlayerFactory = () => {
+    const setPlayerChoice = () => {
+        // Logic to assign player a marker
+        const buttonContainer = document.getElementById("buttonContainer");
+
+        buttonContainer.addEventListener("click", (e) => {
+            if (e.target.id == "markerX"){
+                playerChoice = "x";
+            }
+            else if (e.target.id == "markerY"){
+                playerChoice = "y";
+            }
+            console.log(playerChoice)
+        })
+    }
+    const getPlayerChoice = () =>{
+        return playerChoice;   
+    }
+
+    let playerChoice = "-1";
+    return {setPlayerChoice, getPlayerChoice};
+}
+
+// returns an AI object based on the computer choice
+const AIFactory = (playerChoice) => {
+    const botChoice = () => {
+        if (playerChoice == "x"){
+            return "o";
+        }
+        else{
+            return "x";
+        }
+    }
+    return {botChoice};
+}
+
 console.log(gameBoard.gameBoardArray);
+
+const player = PlayerFactory();
+player.setPlayerChoice();
+const computer = AIFactory(player.getPlayerChoice());
+computer.botChoice();
+
+console.log("Player choice is: " + player.getPlayerChoice())
+console.log(computer)
